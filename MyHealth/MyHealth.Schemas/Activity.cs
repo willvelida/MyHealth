f820186312439de98e8df8a85bb0f59b59c3fc7c
willvelida <willvelida@hotmail.co.uk>
2021-01-02 12:22:21 +1300

    Adding new schemas package. Removing model from Helpers package and incremting version

--- MyHealth/MyHealth.Schemas/Activity.cs
+++ MyHealth/MyHealth.Schemas/Activity.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace MyHealth.Helpers.Models
+namespace MyHealth.Schemas
 {
     public class Activity
     {
---
+++ MyHealth/MyHealth.Schemas/Sleep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHealth.Schemas
+{
+    public class Sleep
+    {
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public int MinutesAsleep { get; set; }
+        public int MinutesAwake { get; set; }
+        public int NumberOfAwakenings { get; set; }
+        public int TimeInBed { get; set; }
+        public int MinutesREMSleep { get; set; }
+        public int MinutesLightSleep { get; set; }
+        public int MinutesDeepSleep { get; set; }
+    }
+}
